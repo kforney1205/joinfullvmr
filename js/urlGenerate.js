1a7d73a9a7cf475a3b8e21f1189b24e28d0acec5
kforney1205 <kyle.forney@gmail.com>
2022-02-16 16:53:52 -0600

    hanges

--- js/urlGenerate.js
+++ js/urlGenerate.js
@@ -1,7 +1,7 @@
 
 function urlGen(f){
    var i1 = 'https://kforney1205.github.io/agentjoin/kiosk.html?';
-   var i2 = 'conference=mp'
+   var i2 = 'conference'
    var i3 = document.getElementById('videoID').value;
    f.action = i1 + i2 + i3;
    return true;
