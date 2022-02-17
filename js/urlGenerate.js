f5aa7883fa2bcde219a690b04bbb20fba1cc81a1
kforney1205 <kyle.forney@gmail.com>
2022-02-16 18:02:36 -0600

    remove unnecessary js src files

--- js/urlGenerate.js
+++
@@ -1,8 +0,0 @@
-
-function urlGen(f){
-   var i1 = 'https://kforney1205.github.io/agentjoin/kiosk.html?';
-   var i2 = 'conference'
-   var i3 = document.getElementById('videoID').value;
-   f.action = i1 + i2 + i3;
-   return true;
-}
